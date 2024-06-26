bd2168548c3b5d336246e94c3d16c656c0e20dfd
Chris <zhris@zhr.is>
2024-06-26 10:20:31 -0400

    Initial Commit

---
+++ WWDC24SessionIntro/WWDC24SessionIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC24SessionIntroApp.swift
+//  WWDC24SessionIntro
+//
+//  Created by Chris on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WWDC24SessionIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
